b9d9d76b53e6a29b78b1e2ad1855bfdf28028df6
00knowah <noahdagmawiwork@gmail.com>
2025-11-15 14:12:11 -0800

    Update SDSU logo in app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ if "code" in qs:
 st.markdown(
     """
     <div style="display:flex; align-items:center; gap:15px; margin-bottom:10px;">
-       <img src="https://brand.sdsu.edu/_images/_logo-section/downloads/1000-horizontal-full-color.jpg" width="500">
+       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/San_Diego_State_Aztecs_logo.svg/2560px-San_Diego_State_Aztecs_logo.svg.png" width="500">
         <h1 style="margin:0;">Room Reserve</h1>
     </div>
     """,
